6cde77eea55551e5598e00359299863db083a579
tyomhk <cheertrain15@naver.com>
2021-12-23 20:40:05 +0900

    Minimized unused variables.

--- src/server.js
+++ src/server.js
@@ -9,10 +9,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 // router
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
 
 // Redirect to home when accessing to unregistered route.
-app.get('/*', (req, res) => res.redirect('/'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 // Turn the server on
 app.listen(9999, () => {console.log('Activated the server.');});
+
